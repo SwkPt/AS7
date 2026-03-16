d1aad71c881a33a289ddef2062cdf73a1a120e05
SwkPt <swk.pt16@gmail.com>
2026-03-16 22:22:22 +0700

    fix banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -23,18 +23,21 @@ export default function Banner(){
     };
 
     return(
-        <div className='relative h-[80vh] w-full overflow-hidden'>
-            <div className='cursor-pointer' onClick={handleImageClick}>
-                <Image src={bannerImages[currentImageIndex]}
-                alt='cover'
-                fill={true}
-                className='object-cover'
-                />
-            </div>
-            <div className='absolute inset-0 bg-black/30' />
-            <div className='relative z-20 flex h-full flex-col items-center justify-center px-4 text-center text-white cursor-pointer' onClick={handleNavigate}>
+        <div className='relative h-[80vh] w-full overflow-hidden cursor-pointer' onClick={handleImageClick}>
+            <Image src={bannerImages[currentImageIndex]}
+            alt='cover'
+            fill={true}
+            className='object-cover'
+            />
+            <div className='absolute inset-0 bg-black/30 pointer-events-none' />
+            <div className='relative z-20 flex h-full flex-col items-center justify-center px-4 text-center text-white pointer-events-none'>
                 <h1 className='text-3xl font-bold tracking-wide sm:text-5xl'>where every event finds its venue</h1>
-                <h3 className='mt-4 text-base sm:text-xl'>Select Venue</h3>
+                <h3 className='mt-4 text-base sm:text-xl'>Finding the perfect venue for all sort of event!!!</h3>
+            </div>
+            <div className='absolute bottom-5 right-5 z-30'>
+                <button onClick={(e) => {e.stopPropagation(); handleNavigate();}} className='bg-white text-black px-6 py-3 rounded-lg font-semibold cursor-pointer hover:bg-gray-200'>
+                    Select Venue
+                </button>
             </div>
         </div>
     );
